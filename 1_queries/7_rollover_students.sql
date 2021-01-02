a861b3e3b8f794d8d9357b552ed14c9eef6460b5
Khaoula <khaoula.mouanniss@gmail.com>
2021-01-02 23:59:42 +0000

    add query with join

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,4 @@
+SELECT students.name, students.start_date, cohorts.name, cohorts.start_date
+FROM students JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE students.start_date != cohorts.start_date
+ORDER BY cohorts.start_date;
