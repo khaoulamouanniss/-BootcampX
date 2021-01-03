956db21d0116c648ddb476f04f450d5f940fc867
Khaoula <khaoula.mouanniss@gmail.com>
2021-01-03 00:27:34 +0000

    add join queries

---
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -0,0 +1,3 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions INNER JOIN students ON students.id = assignment_submissions.student_id
+WHERE students.name = 'Ibrahim Schimmel';
