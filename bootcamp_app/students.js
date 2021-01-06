e1ba93d1e4b164afa611ad72a4df8441d116b779
Khaoula <khaoula.mouanniss@gmail.com>
2021-01-06 20:10:58 +0000

    add the students.js and teachers.js files

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -4,15 +4,27 @@ const pool = new Pool({
   user: 'vagrant',
   password: 'yayaiyad',
   host: 'localhost',
-  database: 'bootcampx'
+  database: 'bootcampx',
+  port: 5432
 });
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
+pool.connect();
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
 .then(res => {
-  console.log(res.rows);
-})
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })})
 .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,14 +1,34 @@
-pool.query(`
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: 'yayaiyad',
+  host: 'localhost',
+  database: 'bootcampx',
+  port: 5432
+});
+
+pool.connect();
+
+const queryString =  `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [cohortName];
+pool.query(queryString, values)
 .then(res => {
+  console.log('line 44')
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
+
